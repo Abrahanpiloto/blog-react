8370126ff0b1f9724baed5cb21b92db8f67bceb9
abrahan <abrahanpiloto@gmail.com>
2024-02-18 06:51:02 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { ButtonList } from "./components/ButtonList";
+import { ArticleList } from "./components/ArticleList";
+
+function App() {
+	return (
+		<>
+			<h1>hola</h1>
+			<ButtonList />
+			<ArticleList />
+		</>
+	);
+}
+
+export default App;
